c3d4eeacb83fe96b2715894d0432a98dd1dd0ee9
Fabricio Anibal <fabricio_anibal@hotmail.com>
2020-04-29 13:26:55 -0300

    CRUD Scaaffolding

---
+++ CursoWebMVC/Data/CursoWebMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CursoWebMVC.Models;
+
+namespace CursoWebMVC.Data
+{
+    public class CursoWebMVCContext : DbContext
+    {
+        public CursoWebMVCContext (DbContextOptions<CursoWebMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CursoWebMVC.Models.Department> Department { get; set; }
+    }
+}
--- CursoWebMVC/Startup.cs
+++ CursoWebMVC/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using CursoWebMVC.Data;
 
 namespace CursoWebMVC
 {
@@ -33,6 +35,9 @@ namespace CursoWebMVC
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<CursoWebMVCContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("CursoWebMVCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
