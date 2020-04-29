99b00f7f501a97449fd4cdfca3d9f68c8ecc5b12
Fabricio Anibal <fabricio_anibal@hotmail.com>
2020-04-29 15:01:17 -0300

    MySQL adaptation and first migration

--- CursoWebMVC/Startup.cs
+++ CursoWebMVC/Startup.cs
@@ -37,7 +37,8 @@ namespace CursoWebMVC
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<CursoWebMVCContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("CursoWebMVCContext")));
+                    options.UseMySql(Configuration.GetConnectionString("CursoWebMVCContext"), builder =>
+                        builder.MigrationsAssembly("CursoWebMVC")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
