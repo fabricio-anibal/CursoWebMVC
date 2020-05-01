33aa01429a4a4431bea7a86f61b27f416f3f005b
Fabricio Anibal <fabricio_anibal@hotmail.com>
2020-05-01 11:55:34 -0300

    FK not null

--- CursoWebMVC/Migrations/20200501144921_DepartmentFK.Designer.cs
+++ CursoWebMVC/Migrations/20200501144921_DepartmentFK.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using CursoWebMVC.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CursoWebMVC.Migrations
 {
     [DbContext(typeof(CursoWebMVCContext))]
-    partial class CursoWebMVCContextModelSnapshot : ModelSnapshot
+    [Migration("20200501144921_DepartmentFK")]
+    partial class DepartmentFK
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
