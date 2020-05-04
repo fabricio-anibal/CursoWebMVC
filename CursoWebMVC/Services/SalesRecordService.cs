77f654598cc5010a7e55190b036182b2d8592fae
Fabricio Anibal <fabricio_anibal@hotmail.com>
2020-05-04 18:06:32 -0300

    Implementing simple search

---
+++ CursoWebMVC/Services/SalesRecordService.cs
@@ -0,0 +1,33 @@
+﻿using CursoWebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CursoWebMVC.Services
+{
+    public class SalesRecordService
+    {
+        private readonly CursoWebMVCContext _context;
+
+        public SalesRecordService(CursoWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if(maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate);
+            }
+            return await result.Include(x => x.Saller).Include(x => x.Saller.Department).OrderByDescending(x => x.Date).ToListAsync();
+        }
+    }
+}
--- CursoWebMVC/Startup.cs
+++ CursoWebMVC/Startup.cs
@@ -47,6 +47,7 @@ namespace CursoWebMVC
             services.AddScoped<SeedingService>();
             services.AddScoped<SallerService>();
             services.AddScoped<DepartmentService>();
+            services.AddScoped<SalesRecordService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
